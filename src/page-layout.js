644d031fa0fff014e70adc889fe152fa05e22a72
Villy Belmonte <vjbbelmont@gmail.com>
2023-06-15 19:14:34 -0700

    Implement Current Forecast section

--- src/page-layout.js
+++ src/page-layout.js
@@ -41,8 +41,98 @@ function createNavigationMenu() {
   return nav;
 }
 
+function createCurrentForecast() {
+  const section = document.createElement('section');
+  section.id = 'current-forecast';
+
+  const currentCityDiv = document.createElement('div');
+  const currentCity = document.createElement('h1');
+  currentCity.id = 'current-city';
+  currentCity.textContent = 'Ventura';
+  currentCityDiv.appendChild(currentCity);
+
+  const currentDetailsDiv = document.createElement('div');
+  currentDetailsDiv.classList.add('forecast-details');
+
+  const currentWeatherDiv = document.createElement('div');
+  const currentWeatherImg = document.createElement('img');
+  currentWeatherImg.id = 'current-weather';
+  currentWeatherImg.src = '../src/assets/images/forecast-current/partly-cloudy.svg';
+  currentWeatherDiv.appendChild(currentWeatherImg);
+
+  const currentTempDiv = document.createElement('div');
+  const currentTemp = document.createElement('h1');
+  currentTemp.classList.add('bold');
+  currentTemp.id = 'current-temp';
+  currentTemp.textContent = '74°F';
+  currentTempDiv.appendChild(currentTemp);
+
+  const forecastDescriptionDiv = document.createElement('div');
+  const forecastDescription = document.createElement('h4');
+  forecastDescription.classList.add('bold');
+  forecastDescription.id = 'forecast-description';
+  forecastDescription.textContent = 'Partly cloudy';
+  forecastDescriptionDiv.appendChild(forecastDescription);
+
+  const highLowDiv = document.createElement('div');
+  highLowDiv.classList.add('high-low');
+
+  const highDiv = document.createElement('div');
+  const highP = document.createElement('p');
+  highP.textContent = 'H: ';
+
+  const high = document.createElement('span');
+  high.id = 'current-high';
+  high.textContent = '74°';
+  highP.appendChild(high);
+  highDiv.appendChild(highP);
+
+  const lowDiv = document.createElement('div');
+  const lowP = document.createElement('p');
+  lowP.textContent = 'L: ';
+
+  const low = document.createElement('span');
+  low.id = 'current-low';
+  low.textContent = '54°';
+  lowP.appendChild(low);
+  lowDiv.appendChild(lowP);
+
+  highLowDiv.appendChild(highDiv);
+  highLowDiv.appendChild(lowDiv);
+
+  currentDetailsDiv.appendChild(currentWeatherDiv);
+  currentDetailsDiv.appendChild(currentTempDiv);
+  currentDetailsDiv.appendChild(forecastDescriptionDiv);
+  currentDetailsDiv.appendChild(highLowDiv);
+
+  section.appendChild(currentCityDiv);
+  section.appendChild(currentDetailsDiv);
+
+  return section;
+}
+
+function createMoreCurrentForecastDetails() {
+  const section = document.createElement('section');
+  section.id = 'more-current-forecast-details';
+
+  const div = document.createElement('div');
+
+  const corDiv = document.createElement('div');
+
+  const corIconDiv = document.createElement('div');
+  const corImg = document.createElement('img');
+  corImg.src = '../src/assets/images/forecast-details/droplet-svgrepo-com.svg';
+  corIconDiv.appendChild(corImg);
+
+  const corPercentDiv = document.createElement('div');
+  const corPercent = document.createElement('p');
+}
+
 export default function createPage() {
   const body = document.getElementsByTagName('body')[0];
   const navigationMenu = createNavigationMenu();
+  const currentForecast = createCurrentForecast();
+
   body.appendChild(navigationMenu);
+  body.appendChild(currentForecast);
 }
