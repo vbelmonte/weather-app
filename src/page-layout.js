d90d64e5d715e42f8e09d07b13b572580a377dff
Villy Belmonte <vjbbelmont@gmail.com>
2023-06-16 19:28:10 -0700

    Implement Additional Current Forecast Details section

--- src/page-layout.js
+++ src/page-layout.js
@@ -111,28 +111,173 @@ function createCurrentForecast() {
   return section;
 }
 
-function createMoreCurrentForecastDetails() {
-  const section = document.createElement('section');
-  section.id = 'more-current-forecast-details';
-
-  const div = document.createElement('div');
-
+function createChanceOfRain() {
   const corDiv = document.createElement('div');
+  corDiv.classList.add('cor');
+  corDiv.classList.add('module');
 
   const corIconDiv = document.createElement('div');
   const corImg = document.createElement('img');
   corImg.src = '../src/assets/images/forecast-details/droplet-svgrepo-com.svg';
   corIconDiv.appendChild(corImg);
+  corDiv.appendChild(corIconDiv);
+
+  const containerDiv = document.createElement('div');
 
   const corPercentDiv = document.createElement('div');
   const corPercent = document.createElement('p');
+  corPercent.classList.add('bold');
+  corPercent.id = 'chance-of-rain';
+  corPercent.textContent = '0%';
+  corPercentDiv.appendChild(corPercent);
+  containerDiv.appendChild(corPercentDiv);
+
+  const corTitleDiv = document.createElement('div');
+  const corTitle = document.createElement('h5');
+  corTitle.classList.add('small');
+  corTitle.classList.add('secondary');
+  corTitle.textContent = 'Rain';
+  corTitleDiv.appendChild(corTitle);
+  containerDiv.appendChild(corTitleDiv);
+
+  corDiv.appendChild(containerDiv);
+
+  return corDiv;
+}
+
+function createHumidity() {
+  const humidityDiv = document.createElement('div');
+  humidityDiv.classList.add('humidity');
+  humidityDiv.classList.add('module');
+
+  const humidityIconDiv = document.createElement('div');
+  const humidityImg = document.createElement('img');
+  humidityImg.src = '../src/assets/images/forecast-details/waves-fill-svgrepo-com.svg';
+  humidityIconDiv.appendChild(humidityImg);
+  humidityDiv.appendChild(humidityIconDiv);
+
+  const containerDiv = document.createElement('div');
+
+  const humidityPercentDiv = document.createElement('div');
+  const humidityPercent = document.createElement('p');
+  humidityPercent.classList.add('bold');
+  humidityPercent.id = 'humidity';
+  humidityPercent.textContent = '20%';
+  humidityPercentDiv.appendChild(humidityPercent);
+  containerDiv.appendChild(humidityPercentDiv);
+
+  const humidityTitleDiv = document.createElement('div');
+  const humidityTitle = document.createElement('h5');
+  humidityTitle.classList.add('small');
+  humidityTitle.classList.add('secondary');
+  humidityTitle.textContent = 'Humidity';
+  humidityTitleDiv.appendChild(humidityTitle);
+  containerDiv.appendChild(humidityTitleDiv);
+
+  humidityDiv.appendChild(containerDiv);
+
+  return humidityDiv;
+}
+
+function createWind() {
+  const windDiv = document.createElement('div');
+  windDiv.classList.add('wind');
+  windDiv.classList.add('module');
+
+  const windIconDiv = document.createElement('div');
+  const windImg = document.createElement('img');
+  windImg.src = '../src/assets/images/forecast-details/wind-fill-svgrepo-com.svg';
+  windIconDiv.appendChild(windImg);
+  windDiv.appendChild(windIconDiv);
+
+  const containerDiv = document.createElement('div');
+
+  const windSpeedDiv = document.createElement('div');
+  const windSpeed = document.createElement('p');
+  windSpeed.classList.add('bold');
+  windSpeed.id = 'wind';
+  windSpeed.textContent = '5mph';
+  windSpeedDiv.appendChild(windSpeed);
+  containerDiv.appendChild(windSpeedDiv);
+
+  const windTitleDiv = document.createElement('div');
+  const windTitle = document.createElement('h5');
+  windTitle.classList.add('small');
+  windTitle.classList.add('secondary');
+  windTitle.textContent = 'Wind';
+  windTitleDiv.appendChild(windTitle);
+  containerDiv.appendChild(windTitleDiv);
+
+  windDiv.appendChild(containerDiv);
+
+  return windDiv;
+}
+
+function createUV() {
+  const uvDiv = document.createElement('div');
+  uvDiv.classList.add('uv');
+  uvDiv.classList.add('module');
+
+  const uvIconDiv = document.createElement('div');
+  const uvImg = document.createElement('img');
+  uvImg.src = '../src/assets/images/forecast-details/sun-filled-svgrepo-com.svg';
+  uvIconDiv.appendChild(uvImg);
+  uvDiv.appendChild(uvIconDiv);
+
+  const containerDiv = document.createElement('div');
+
+  const uvIndexDiv = document.createElement('div');
+  const uvIndex = document.createElement('p');
+  uvIndex.classList.add('bold');
+  uvIndex.id = 'uv-index';
+  uvIndex.textContent = '5 of 10';
+  uvIndexDiv.appendChild(uvIndex);
+  containerDiv.appendChild(uvIndexDiv);
+
+  const uvTitleDiv = document.createElement('div');
+  const uvTitle = document.createElement('h5');
+  uvTitle.classList.add('small');
+  uvTitle.classList.add('secondary');
+  uvTitle.textContent = 'UV Index';
+  uvTitleDiv.appendChild(uvTitle);
+  containerDiv.appendChild(uvTitleDiv);
+
+  uvDiv.appendChild(containerDiv);
+
+  return uvDiv;
+}
+
+function createMoreCurrentForecastDetails() {
+  const section = document.createElement('section');
+  section.id = 'more-current-forecast-details';
+
+  const moreForecastDetailsDiv = document.createElement('div');
+  moreForecastDetailsDiv.id = 'more-current-forecast-details-container';
+
+  const chanceOfRain = createChanceOfRain();
+  moreForecastDetailsDiv.appendChild(chanceOfRain);
+
+  const humidity = createHumidity();
+  moreForecastDetailsDiv.appendChild(humidity);
+
+  const wind = createWind();
+  moreForecastDetailsDiv.appendChild(wind);
+
+  const uv = createUV();
+  moreForecastDetailsDiv.appendChild(uv);
+
+  section.appendChild(moreForecastDetailsDiv);
+
+  return section;
 }
 
 export default function createPage() {
   const body = document.getElementsByTagName('body')[0];
   const navigationMenu = createNavigationMenu();
   const currentForecast = createCurrentForecast();
+  const moreCurrentForecastDetails = createMoreCurrentForecastDetails();
 
   body.appendChild(navigationMenu);
   body.appendChild(currentForecast);
+  body.appendChild(moreCurrentForecastDetails);
 }
