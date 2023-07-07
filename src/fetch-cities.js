f9363b3e5ffb745196129b790627aebde1c3a32a
Villy Belmonte <vjbbelmont@gmail.com>
2023-07-07 15:01:33 -0700

    Modify search functions for city and postal code

--- src/fetch-cities.js
+++ src/fetch-cities.js
@@ -1,38 +1,37 @@
-async function checkByPostalCode(postalCode, countryCode) {
+function splitQuery(query) {
+  const newQuery = query.split(',');
+  return newQuery;
+}
+
+async function checkByPostalCode(query) {
   try {
-    let response;
-    if (countryCode === undefined) {
-      response = await fetch(`https://api.openweathermap.org/geo/1.0/zip?zip=${postalCode}&appid=8dd447f0efae8324f15c24f2efdd7e9f`, { mode: 'cors' });
-    } else {
-      response = await fetch(`https://api.openweathermap.org/geo/1.0/zip?zip=${postalCode},${countryCode}&appid=8dd447f0efae8324f15c24f2efdd7e9f`, { mode: 'cors' });
-    }
+    const response = await fetch(`https://api.openweathermap.org/geo/1.0/zip?zip=${query}&appid=8dd447f0efae8324f15c24f2efdd7e9f`, { mode: 'cors' });
+
     if (!response.ok) {
       throw new Error(`${response.status}, ${response.statusText}`);
     }
+
     const result = await response.json();
     return result;
+
   } catch (error) {
     // return error
     console.log(error);
-    return error;
+    return error;0
   }
 }
 
-async function checkByCity(city, state, country) {
+async function checkByCity(query) {
   try {
-    let response;
-    if (state === undefined && country === undefined) {
-      response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=8dd447f0efae8324f15c24f2efdd7e9f`, { mode: 'cors' });
-    } else if (state === undefined) {
-      response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=5&appid=8dd447f0efae8324f15c24f2efdd7e9f`, { mode: 'cors' });
-    } else {
-      response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=5&appid=8dd447f0efae8324f15c24f2efdd7e9f`, { mode: 'cors' });
-    }
+    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=8dd447f0efae8324f15c24f2efdd7e9f`, { mode: 'cors' });
+
     if (!response.ok) {
       throw new Error(`${response.status}, ${response.statusText}`);
     }
+
     const result = await response.json();
     return result;
+
   } catch (error) {
     // return error
     console.log(error);
@@ -41,8 +40,9 @@ async function checkByCity(city, state, country) {
 }
 
 function checkQuery(query) {
-  /**
-     * break up query into correct parts (eg. city, state, country, zip code)
+  // break up query into correct parts (eg. city, state, country, zip code)
+  const newQuery = splitQuery(query);
+  /*
      * check by postal code
      * if postal code fulfills, return city and city data
      * if postal code fails, check by city name
