0818de1af78cdfb24fc2d6f66b2e113123b21ec7
Villy Belmonte <vjbbelmont@gmail.com>
2023-07-17 18:03:05 -0700

    Implement fetchAdditionalWeatherDetails

--- src/fetch-weather.js
+++ src/fetch-weather.js
@@ -19,33 +19,50 @@ async function getWeatherData(lat, lon, cityName) {
 async function getUVIndex(query) {
   const latitude = query.lat;
   const longitude = query.lon;
-  const result = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=uv_index_max&timezone=auto`);
+  try {
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=uv_index_max&timezone=auto`);
 
-  return result;
+    if (!response.ok) {
+      throw new Error(`${response.status}, ${response.statusText}`);
+    }
+    const result = await response.json();
+    const UVIndex = Math.floor(result.daily.uv_index_max[0]);
+
+    return UVIndex;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 }
 
 async function getChanceOfRain(query) {
   const latitude = query.lat;
   const longitude = query.lon;
-  const result = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=precipitation_probability_max&timezone=auto`);
+  try {
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=precipitation_probability_max&timezone=auto`);
 
-  return result;
+    if (!response.ok) {
+      throw new Error(`${response.status}, ${response.statusText}`);
+    }
+    const result = await response.json();
+    const chanceOfRain = result.daily.precipitation_probability_max[0];
+
+    return chanceOfRain;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 }
 
-async function getHourlyForecast(query) {
-  const latitude = query.lat;
-  const longitude = query.lon;
-  const result = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weathercode&temperature_unit=fahrenheit&timezone=auto`);
+async function getAdditionalWeatherDetails(query) {
+  const UVIndex = await getUVIndex(query);
+  const chanceOfRain = await getChanceOfRain(query);
 
-  return result;
-}
+  const additionalForecastDetails = {
+    chance_of_rain: chanceOfRain, uv_index: UVIndex,
+  };
 
-async function getThreeDayForecast(query) {
-  const latitude = query.lat;
-  const longitude = query.lon;
-  const result = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_probability_max&temperature_unit=fahrenheit&timezone=auto&forecast_days=3`);
-
-  return result;
+  return additionalForecastDetails;
 }
 
 async function fetchCurrentWeather(query) {
@@ -58,8 +75,46 @@ async function fetchCurrentWeather(query) {
   return weatherData;
 }
 
+async function getHourlyForecast(query) {
+  const latitude = query.lat;
+  const longitude = query.lon;
+  try {
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weathercode&temperature_unit=fahrenheit&timezone=auto`);
+
+    if (!response.ok) {
+      throw new Error(`${response.status}, ${response.statusText}`);
+    }
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+async function getThreeDayForecast(query) {
+  const latitude = query.lat;
+  const longitude = query.lon;
+  try {
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_probability_max&temperature_unit=fahrenheit&timezone=auto&forecast_days=3`);
+
+    if (!response.ok) {
+      throw new Error(`${response.status}, ${response.statusText}`);
+    }
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
 export default async function fetchWeather(query) {
   const currentWeatherData = await fetchCurrentWeather(query);
+  const additionalCurrentWeatherData = await getAdditionalWeatherDetails(query);
+  const data = { ...currentWeatherData, ...additionalCurrentWeatherData };
 
-  return currentWeatherData;
+  return data;
 }
